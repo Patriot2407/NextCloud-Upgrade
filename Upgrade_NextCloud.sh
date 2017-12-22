c6be209415938e6a29243b59b2debc8974f89cd1
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-22 14:32:52 -0500

    Ver 1.0
    Finishing up script

--- Upgrade_NextCloud.sh
+++ Upgrade_NextCloud.sh
@@ -5,6 +5,7 @@ NCDB="ncdb"
 NCUSER="ncuser"
 NCUSERPASS="$(openssl rand -base64 12)"
 NCDIR="/var/www/html/nextcloud"
+NCDIRNEW="/var/www/html/nextcloud/"
 clear
 #----------------------------- User Input --------------------------------------------------
 echo "Please enter root user MySQL password... (Password type prompt is hidden)"
@@ -14,14 +15,29 @@ echo "Entering custom parameters..."
 read -e -p "Desired database name for NextCloud... default is [$NCDB]: " -i "$NCDB" NCDB
 read -e -p "Desired user name for NextCloud... default is [$NCUSER]: " -i "$NCUSER" NCUSER
 read -e -p "Desired password for NextCloud user... default is [$NCUSERPASS]: " -i "$NCUSERPASS" NCUSERPASS
-read -e -p "Desired password for NextCloud user... default is [$NCDIR]: " -i "$NCDIR" NCDIR
+read -e -p "NextCloud location... default is [$NCDIR]: " -i "$NCDIR" NCDIR
 # Backup sequence start
 sudo mkdir /Backups
 mysqldump -hlocalhost -uroot -p${rootpasswd} ${NCDB}| gzip > /Backups/NextCloud_db_Backup.zip
 mysqldump -hlocalhost -uroot -p${rootpasswd} ${NCDB} > /Backups/nextcloud_Backup.sql
 sudo tar -cpzvf /Backups/nextcloud-config.tar.gz $NCDIR/config/
 sudo tar -cpvzf /Backups/nextcloud-data.tar.gz $NCDIR/data
+sudo tar -cpvzf /Backups/nextcloud-apacheconfigs.tar.gz /etc/apache2/sites-available/nextcloud.conf
 # Backup sequence finish
-
+# Start Upgrade Sequence
+mv $NCDIR $NCDIR-old
+sudo mkdir /tmp
+cd /tmp
+wget https://download.nextcloud.com/server/releases/nextcloud-$NCVERSION.zip
+echo "Finished."
+echo "Unzipping NextCloud Binaries..."
+unzip nextcloud-$NCVERSION.zip
+sudo cp -r nextcloud/ /var/www/html/
+sudo chown -R www-data:www-data $NCDIRNEW
+sudo cp $NCDIR-old/config/config.php $NCDIRNEW/config/
+sudo cp -r $NCDIR-old/data $NCDIRNEW/data
+sudo systemctl restart apache2
+cd /var/www/html/nextcloud
+sudo -u www-data php occ upgrade
 echo "Open up your web browser and navigate to URL: http://$IPADD/nextcloud." 
 exit 0
